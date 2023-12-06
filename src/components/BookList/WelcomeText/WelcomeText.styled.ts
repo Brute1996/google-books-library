5f8c360314f7ba613a49acf2c010fe39f9fac916
Sergey <sergey.cherenkov96@gmail.com>
2023-12-06 19:49:31 +0200

    fix style for welcome text

--- src/components/BookList/WelcomeText/WelcomeText.styled.ts
+++ src/components/BookList/WelcomeText/WelcomeText.styled.ts
@@ -4,9 +4,5 @@ export const WelcomeTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  margin-top: 50px;
 `;
